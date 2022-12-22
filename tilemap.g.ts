39fb99d17ebd993b2e7270e2a9935c4c2ccdc014
Charles-Pritchard <116393516+Charles-Pritchard@users.noreply.github.com>
2022-12-22 14:31:35 +1100

    enemys and bonus points

--- tilemap.g.ts
+++ tilemap.g.ts
@@ -6,7 +6,7 @@ namespace myTiles {
     helpers._registerFactory("tilemap", function(name: string) {
         switch(helpers.stringTrim(name)) {
             case "level1":
-            case "level1":return tiles.createTilemap(hex`100010000e0e0e08080808080808080808080e0e0e07070e07070707070707070e0e070e0e05050e0505050505050e0e0505050e0e03030e03030505050e060202020e020e030e030e030104040e040404040e010e030e030e030104040e0404040e0401030e0e0e0e0e0104040e0404040e04010c0c0e0e0e0c0e0e0e0e0e04040e04010d0e0e0e0e0c01040e0e0e0e0e0e0e010d0e0e0e0e0a0104040404040e040e010d0e0e0e0e0a01040404040e040404010d0e0b0e0b0a010404040e04040404010d0e0a0e0a0a0104040e0404040404010d0e0e0e0b0a01040e040404040404010d0a0a0a0a0a010e04040404040404090d0a0a0a0a0e0e040404040404040401`, img`
+            case "level1":return tiles.createTilemap(hex`100010000d0707070707070707070707070707050e0a0a0c030303030303030d0a0a0b080e170a11130202120404040e0a0a0a080e0a0a11011616010404040e0a0b0a080f030310011616010404040e0a0a0a0804040404011616010404040e0a0a0a0813020202141616010404040e0b0a170815121616161616010404040e0a0a0a0813141616161616010404040e0a0a0a0b15121616161616010404040e0a0a0a0804011616161616010404040f0d0b0a080401161616161601040404040e0a0a080401161616161601040404040e0a17080401161616161601040404040e0a0a080401161616161601040404040e0b0a080415020202021201040404040e090906`, img`
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
@@ -23,7 +23,7 @@ namespace myTiles {
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
-`, [myTiles.transparency16,sprites.vehicle.roadVertical,sprites.vehicle.roadHorizontal,sprites.dungeon.doorClosedSouth,sprites.dungeon.floorLight1,sprites.dungeon.greenOuterNorth0,sprites.dungeon.stairLadder,sprites.dungeon.stairSouth,sprites.dungeon.purpleOuterSouth0,sprites.dungeon.purpleSwitchDown,sprites.builtin.oceanSand14,sprites.castle.saplingOak,sprites.builtin.brick,sprites.dungeon.collectibleBlueCrystal,sprites.dungeon.collectibleInsignia], TileScale.Sixteen);
+`, [myTiles.transparency16,sprites.vehicle.roadVertical,sprites.vehicle.roadHorizontal,sprites.dungeon.greenOuterNorth0,sprites.builtin.brick,sprites.castle.tilePath3,sprites.castle.tilePath9,sprites.castle.tilePath2,sprites.castle.tilePath6,sprites.castle.tilePath8,sprites.castle.tilePath5,sprites.swamp.swampTile2,sprites.dungeon.greenOuterNorthWest,sprites.dungeon.greenOuterNorthEast,sprites.dungeon.greenOuterEast0,sprites.dungeon.greenInnerSouthWest,sprites.dungeon.greenInnerSouthEast,sprites.dungeon.greenOuterWest1,sprites.vehicle.roadTurn2,sprites.vehicle.roadTurn1,sprites.vehicle.roadTurn4,sprites.vehicle.roadTurn3,sprites.dungeon.hazardLava0,sprites.builtin.forestTiles0], TileScale.Sixteen);
         }
         return null;
     })
